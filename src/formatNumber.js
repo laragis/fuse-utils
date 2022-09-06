c90e4d2bda6a8bfc99f497c0f178c465ded7f6b6
Trương Thanh Tùng <ttungbmt@gmail.com>
2022-09-06 15:42:13 +0700

    Update changes

---
+++ src/formatNumber.js
@@ -0,0 +1,9 @@
+import numeral from 'numeral'
+
+function formatNumber(number, format = '0,0'){
+  return numeral(number).format(format)
+}
+
+export default formatNumber
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as env} from './env'
 export {default as assets} from './assets'
+export {default as formatNumber} from './formatNumber'
